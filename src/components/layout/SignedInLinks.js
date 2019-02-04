831ac381c1c4325449a40a8998175a1cfbc8998e
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2019-02-04 23:22:07 +0100

    fill signetoutlinks

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
     return(
        <ul className="right">
-           <li><NavLink to='/'>New Project</NavLink></li>
+           <li><NavLink to='/create'>New Project</NavLink></li>
            <li><NavLink to='/'></NavLink>Log Out</li>
            <li><NavLink to='/' className='btn btn-floating indigo lighten-1'>KS</NavLink></li>
 
---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class CreateProject extends Component {
+  state = {
+    title: '',
+    content: ''
+  };
+
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.id] : e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+   e.preventDefault()
+   console.log(this.state)
+  }
+
+  render() {
+    return (
+      
+        <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+            <h5 className="grey-text text-darken-3">Create new project</h5>
+            <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange}/>
+        </div>
+        <div className="input-field">
+            <label htmlFor="content">Project Content</label>
+            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>      
+            </div>
+            <div className="input-field"></div>
+            <button className="btn blue darken-1 z-depth-0">Create</button>
+        </form>
+        </div>
+    
+      
+    );
+  }
+}
+
+export default CreateProject;
