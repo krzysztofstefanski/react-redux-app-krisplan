9b7fd562e02d233b280dff42da45eca57e127108
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2019-02-06 13:29:51 +0100

    add redux-thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,12 @@
-const initState = {}
+const initState = {
+
+    projects: [
+        {id:1,title:'hel me find dog',content:'bla bla bla'},
+        {id:1,title:'hel me find cat',content:'bla bla bla'},
+        {id:1,title:'hel me find frog',content:'bla bla bla'}
+]
+
+}
 
 const projectReducer = (state = initState, action)=>{
     return state
