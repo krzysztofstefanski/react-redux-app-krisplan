620cfbf1b22823340c547a4aff7b087d3aafa598
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2019-02-10 22:57:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ class App extends Component {
        <Switch>
          <Route exact path='/' component={Dashboard} />
          <Route path='/project /:id' component={ProjectDetails}/>
-         <Route path='/signin' component={SignIn}/>
-         <Route path='/signup' component={SignUp}/>
-         <Route path='/create' component={CreateProject}/>
+         <Route path='/signin ' component={SignIn}/>
+         <Route path='/signup ' component={SignUp}/>
+         <Route path='/create ' component={CreateProject}/>
 
          </Switch>
       </div>
