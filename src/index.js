bd548cad5fe5e3c770fb08557c3df0840d5f605b
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2019-02-14 21:10:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {reduxFirestore,getFirestore} from 'redux-firestore'
 import {reactReduxFirebase,getFirebase} from 'react-redux-firebase'
 import fbConfig from './components/config/fbConfig' 
 
-const store = createStore(rootReducer,
+const store = createStore(rootReducer ,
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
     reduxFirestore(fbConfig),
