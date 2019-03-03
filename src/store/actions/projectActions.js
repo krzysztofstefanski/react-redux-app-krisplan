f734afaa11d4b9c31bc999bfa22d33c587f84b33
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2019-03-03 22:07:40 +0100

    Update projectActions.js

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,6 @@ export const createProject = (project) => {
     return (dispatch, getState , {getFirebase , getFirestore}) => {
         //make async call to database
 
-dispatch({type:'CREATE_PROJECT', project})
+dispatch({type:'CREATE_PROJECT' , project})
     }
 }
